93dbc499a9562f73e8dfafb112e2afe540457989
Pin Lin <moneycat711@gmail.com>
2018-12-12 15:49:44 +0800

    Configure develop envirement

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const logger = require('koa-logger')
+
+const app = new Koa()
+const router = new Router()
+
+app.use(logger())
+
+router.get('/', async ctx => {
+    ctx.body = "Hello China!"
+})
+
+app.use(router.routes())
+
+app.listen(3000)
