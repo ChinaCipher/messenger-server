1e45a14ed6c7cb7879c12755564a866ea2915e03
zxjte9411 <a5852241@gmail.com>
2018-12-18 03:15:08 +0800

    繁體中文化及刪除用不到的 code

--- messenger-server/src/db/models/users.js
+++ messenger-server/src/db/models/users.js
@@ -1,10 +1,9 @@
-// const crypto = require('crypto')
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-//type:字段类型，包括String,Number,Date,Buffer,Boolean,Mixed,ObjectId,Array
-//index:是否索引，注意唯一索引unique的写法
-//default:默认值
+//type:字段類型，包括String,Number,Date,Buffer,Boolean,Mixed,ObjectId,Array
+//index:是否索引，注意唯一索引unique的寫法
+//default:默認值
 const User = new Schema({
     "username": { type: String, index: { unique: true, dropDups: true }, minlength: 8, maxlength: 30},
     "password": { type: String, match: /\w+/, index: true },
@@ -14,28 +13,10 @@ const User = new Schema({
     "publickey": { type: String, index: true },
 })
 
-//使用setter，将用户输入的明文密码sha1之后存储
-// User.path('password').set(function (v) {
-//     let shasum = crypto.createHash('sha1')
-//     shasum.update(v)
-//     return shasum.digest('hex')
-// })
-
-//使用 middleware，每次保存都记录一下最后更新时间
-User.pre('save', function(next) {
-    this.updated = Date.now()
-    next()
-})
-
-//静态方法，按用户名查找，因为username加了唯一索引，
-//所以这里用的是findOne，只查询一条数据
+//靜態方法，按用戶名查找，因為 username 加了唯一索引，
+//所以這裡用的是 findOne，只查詢一條數據
 User.statics.findByUsername = function (username) {
     return this.findOne({ username: username })
-}
-
-User.statics.get_full_name = function (username) {
-    let user = this.findOne({ username: username })
-    return user.username + user.nickname
 }
 
 //创建模型
