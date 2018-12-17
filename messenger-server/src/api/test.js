69b4b48750fa0e053dbf38f49a7f3b9b262527d2
zxjte9411 <a5852241@gmail.com>
2018-12-18 04:36:42 +0800

    write test

--- messenger-server/src/api/test.js
+++ messenger-server/src/api/test.js
@@ -1,9 +1,13 @@
 const Router = require('koa-router')
+const db = require('../db/User')
 
 const router = new Router()
 
+
 router.get('/', async ctx => {
-    ctx.body = {message: "Hello China!"}
+    let zxjte9411 = new db.User(await db.init('zxjte9411'))
+    ctx.body = JSON.stringify(zxjte9411, undefined, 2)
+    // ctx.body = {message: "Hello China!"}
 })
 
 module.exports = {
