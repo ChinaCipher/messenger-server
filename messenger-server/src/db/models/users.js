29d2c5d6a851d8201077becd91fbeba1fea54306
zxjte9411 <a5852241@gmail.com>
2018-12-18 04:35:31 +0800

    fix code

--- messenger-server/src/db/models/users.js
+++ messenger-server/src/db/models/users.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 //type:字段類型，包括String,Number,Date,Buffer,Boolean,Mixed,ObjectId,Array
 //index:是否索引，注意唯一索引unique的寫法
 //default:默認值
-const User = new Schema({
+const users = new Schema({
     "username": { type: String, index: { unique: true, dropDups: true }, minlength: 8, maxlength: 30},
     "password": { type: String, match: /\w+/, index: true },
     "avatar": {type: String, index: true, default: ''},
@@ -15,11 +15,11 @@ const User = new Schema({
 
 //靜態方法，按用戶名查找，因為 username 加了唯一索引，
 //所以這裡用的是 findOne，只查詢一條數據
-User.statics.findByUsername = function (username) {
+users.statics.findByUsername = function (username) {
     return this.findOne({ username: username })
 }
 
 //创建模型
-const model = mongoose.model('User', User)
+const model = mongoose.model('users', users)
 
 module.exports = model
