57c3386fb60841aad8096b06d217c0f98ce0dc51
Pin Lin <moneycat711@gmail.com>
2018-12-19 02:31:27 +0800

    Edit GET / message

--- messenger-server/src/api/test.js
+++ messenger-server/src/api/test.js
@@ -8,7 +8,7 @@ const router = new Router()
 router.get('/', async ctx => {
     ctx.body = {
         version: config.app.version,
-        message: "Hello China!"
+        message: "Hello, China!"
     }
 })
 
