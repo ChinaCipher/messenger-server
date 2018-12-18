88cf73443ac9e48d65848ee9b1a1b699f4876bc7
zxjte9411 <a5852241@gmail.com>
2018-12-18 21:49:25 +0800

    回覆如初

--- messenger-server/src/api/test.js
+++ messenger-server/src/api/test.js
@@ -1,13 +1,9 @@
 const Router = require('koa-router')
-const db = require('../db/User')
 
 const router = new Router()
 
 
 router.get('/', async ctx => {
-    // let doc = await db.findUser('zxjte9411')
-    // let zxjte9411 = new db(doc)
-    // ctx.body = JSON.stringify(zxjte9411, undefined, 2)
     ctx.body = {message: "Hello China!"}
 })
 
