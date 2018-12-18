0528d109587da8834ce65f2034c1da50f349d3e4
zxjte9411 <a5852241@gmail.com>
2018-12-18 20:07:43 +0800

    修改一些 function 為靜態函式、修改一修程式碼，讓它簡潔些

--- messenger-server/src/db/User.js
+++ messenger-server/src/db/User.js
@@ -4,96 +4,54 @@ class User {
     /**
      * @param {Object} user 
      */
-    constructor(user){
+    constructor(user) {
         this._nickname = user.nickname
         this._username = user.username
         this._password = user.password
         this._avatar = user.avatar
-
     }
 
-    set username(value){
-        update(this._username, value, 'username')
+    set username(value) {
+        User._update(this._username, value, 'username')
         this._username = value
     }
 
-    set nickname(value){
-        if (this !== {}) {
-            this._nickname = value
-            update(this._username, value, 'nickname')
-        }
-
+    set nickname(value) {
+        this._nickname = value
+        User._update(this._username, value, 'nickname')
     }
 
-    set password(value){
+    set password(value) {
         this._password = value
-        update(this._username, value, 'password')
+        User._update(this._username, value, 'password')
     }
 
-    set avatar(value){
+    set avatar(value) {
         this._avatar = value
-        update(this._username, value, 'avatar')
+        User._update(this._username, value, 'avatar')
     }
 
-    get username(){
+    get username() {
         return this._username
     }
-    get nickname(){
+    get nickname() {
         return this._nickname
     }
-    get password(){
+    get password() {
         return this._password
     }
-    get avatar(){
+    get avatar() {
         return this._avatar
     }
-}
 
-/**
- * @param {String} username 
- */
-async function getuser(username){
-    return await users.findOne({ 'username': username })
-}
-
-/**
- * @param {String} username 
- * @param {String} nickname 
- */
-async function update(username, value, opction){
-    const query = {'username': username}
-    switch (opction) {
-        case 'username':
-            await users.update(query, { '$set': {'username': value} })
-            break
-        case 'nickname':
-            await users.update(query, { '$set': {'nickname': value} })
-            break
-        case 'password':
-            await users.update(query, { '$set': {'password': value} })
-            break
-        case 'avatar':
-            await users.update(query, { '$set': {'avatar': value} })
-            break
-        default:
-            break
+    static async findUser(username) {
+        return await users.findByUsername(username/*, User.callback*/)
     }
-    // await users.update({ 'username': value }, { '$set': { "nickname": nickname } })
-}
 
-/**
- * @param {String} username 
- */
-async function init(username){
-    const user = await users.findByUsername(username)
-    if (user === null){
-        return null
-    } else {
-        return await user
+    static async _update(username, value, opction) {
+        await users.update({username}, {'$set': {[opction]: value}})
     }
+
 }
 
-module.exports = {
-    User,
-    init
-}
+module.exports = User
