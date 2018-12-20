589300f4e9e7eb8f622dc3ef7da5749d28cad375
Pin Lin <moneycat711@gmail.com>
2018-12-20 16:35:59 +0800

    POST /chat/{username}/message (temp)

--- messenger-server/src/api/chat.js
+++ messenger-server/src/api/chat.js
@@ -134,4 +134,61 @@ router.get('/:username/message', async ctx => {
     }
 })
 
+router.post('/:username/message', async ctx => {
+    const username = ctx.request.body.username
+    const type = ctx.request.body.type
+    const content = ctx.request.body.content
+    const options = ctx.request.body.options
+
+    if (!ctx.session.login) {
+        ctx.body = {
+            message: "not logged in."
+        }
+        ctx.status = 401
+        return
+    }
+
+    let user = await User.find(username)
+    if (!user) {
+        ctx.body = {
+            message: "username does not exist."
+        }
+        ctx.status = 404
+        return
+    }
+
+    // temp
+    if (rooms.username) {
+        ctx.body = {
+            message: "chatroom already exists."
+        }
+        ctx.status = 403
+        return
+    }
+
+    let originalMessageKey = crypto.randomBytes(32)
+
+    let userA = await User.find(ctx.session.username)
+    let userB = user
+
+    // temp
+    let temproom = {
+        userA: {
+            username: [userA.username],
+            messageKey: await ecies.encrypt(userA.publicKey, originalMessageKey)
+        },
+        userB: {
+            username: [userB.username],
+            messageKey: await ecies.encrypt(userB.publicKey, originalMessageKey)
+        },
+        messages: []
+    }
+    temprooms[username] = temproom
+
+    // temp
+    ctx.body = {
+        messageKey: temproom.userA.messageKey
+    }
+})
+
 module.exports = router 
