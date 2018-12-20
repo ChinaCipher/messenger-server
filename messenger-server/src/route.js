47a06ba744441f8fe69f5f953468391d8b590fd8
Pin Lin <moneycat711@gmail.com>
2018-12-20 16:13:30 +0800

    Add chat.js

--- messenger-server/src/route.js
+++ messenger-server/src/route.js
@@ -1,12 +1,14 @@
 const Router = require('koa-router')
 
 const test = require('./api/test')
+const chat = require('./api/chat')
 const user = require('./api/user')
 const session = require('./api/session')
 
 const router = new Router()
 
 router.use('/', test.routes(), test.allowedMethods())
+router.use('/chat', chat.routes(), chat.allowedMethods())
 router.use('/user', user.routes(), user.allowedMethods())
 router.use('/session', session.routes(), session.allowedMethods())
 
