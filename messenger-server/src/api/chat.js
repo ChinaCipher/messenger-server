9080704a1d2cfc3366d8b7eb666f953f79ca15b4
Pin Lin <moneycat711@gmail.com>
2018-12-20 16:14:47 +0800

    GET /chat (temp)

--- messenger-server/src/api/chat.js
+++ messenger-server/src/api/chat.js
@@ -1,6 +1,27 @@
 const Router = require('koa-router')
 
+const User = require('../db/user')
+
 const router = new Router()
 
 
+// temp
+let temprooms = {}
+
+router.get('/', async ctx => {
+    if (!ctx.session.login) {
+        ctx.body = {
+            message: "not logged in."
+        }
+        ctx.status = 401
+        return
+    }
+
+    // temp
+    let rooms = Object.values(temprooms)
+
+    ctx.body = {
+        rooms
+    }
+})
 module.exports = router 
