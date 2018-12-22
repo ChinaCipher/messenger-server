95ddf04c20b1107c486d15866335437607795984
zxjte9411 <a5852241@gmail.com>
2018-12-22 14:10:18 +0800

    修改參數名稱＆增加除了username以外的 gettet 與 setter

--- messenger-server/src/db/message.js
+++ messenger-server/src/db/message.js
@@ -1,8 +1,9 @@
-const message = require('./models/chatrooms')
+const chatrooms = require('./models/chatrooms')
 
 class Message {
     constructor(msgdata, usernameA, usernameB) {
         this._id = msgdata.id
+        this._timestamp = msgdata.timestamp
         this._sender = msgdata.sender
         this._type = msgdata.type
         this._content = msgdata.content
@@ -15,37 +16,59 @@ class Message {
         return this._id
     }
 
-    get content() {
-        return this._content
+    get timestamp() {
+        return this._timestamp
     }
 
-    set content(value) {
-        this._content = value
-        Message._update(value, this._usernameA, this._usernameB, this._id, 'content')
+    get sender() {
+        return this._sender
     }
 
     get type() {
         return this._type
     }
 
-    set type(value) {
-        this._type = value
-        Message._update(value, this._usernameA, this._usernameB, this._id, 'type')
+    get content() {
+        return this._content
     }
-
 
     get options() {
         return this._options
     }
 
+    set id(value) {
+        Message._update(value, this._usernameA, this._usernameB, this._id, 'id')
+        this._id = value
+    }
+
+    set timestamp(value) {
+        Message._update(value, this._usernameA, this._usernameB, this._id, 'timestamp')
+        this._timestamp = value
+    }
+
+    set sender(value) {
+        Message._update(value, this._usernameA, this._usernameB, this._id, 'sender')
+        this._sender = value
+    }
+
+    set type(value) {
+        Message._update(value, this._usernameA, this._usernameB, this._id, 'type')
+        this._type = value
+    }
+
+    set content(value) {
+        Message._update(value, this._usernameA, this._usernameB, this._id, 'content')
+        this._content = value
+    }
+
     set options(value) {
-        this._options = value
         Message._update(value, this._usernameA, this._usernameB, this._id, 'options')
+        this._options = value
     }
 
     static async _update(value, usernameA, usernameB, id, option) {
         let o = "messages.$." + option
-        await message.update({ "userA.username": usernameA, "userB.username": usernameB, "messages.id": id },
+        await chatrooms.update({ "userA.username": usernameA, "userB.username": usernameB, "messages.id": id },
             { "$set": { [o]: value } })
     }
 }
