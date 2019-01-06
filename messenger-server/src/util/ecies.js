173a611398f82313f285ae9592010e926bf9e68e
Pin Lin <moneycat711@gmail.com>
2019-01-06 13:41:44 +0800

    Finished part of util

--- messenger-server/src/util/ecies.js
+++ messenger-server/src/util/ecies.js
@@ -3,18 +3,23 @@ const eccrypto = require('eccrypto')
 
 
 const generateKeyPair = () => {
+    // 亂數產生 EC 私鑰
     let privateKey = crypto.randomBytes(32)
+    // 由 EC 私鑰產生公鑰
     let publicKey = eccrypto.getPublic(privateKey)
 
+    // 以 HEX 格式回傳
     return {
         privateKey: privateKey.toString('hex'),
         publicKey: publicKey.toString('hex')
     }
 }
 
+// 統一 ECIES 加密的配置
 const encrypt = async (key, plaintext) => {
     let ciphertext = await eccrypto.encrypt(Buffer.from(key, 'hex'), Buffer.from(plaintext, 'utf8'))
 
+    // 將密文物件的 Binary 格式資料轉為 HEX 格式
     Object.keys(ciphertext).forEach(key => {
         ciphertext[key] = ciphertext[key].toString('hex')
     });
@@ -22,7 +27,9 @@ const encrypt = async (key, plaintext) => {
     return ciphertext
 }
 
+// 統一 ECIES 解密的配置
 const decrypt = async (key, ciphertext) => {
+    // 將密文物件的 HEX 格式資料轉為 Binary 格式
     Object.keys(ciphertext).forEach(key => {
         ciphertext[key] = Buffer.from(ciphertext[key], 'hex')
     });
