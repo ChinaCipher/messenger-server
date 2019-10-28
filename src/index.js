21d74463b20557892e48b45d07fc198df360b22a
Pin Lin <moneycat711@gmail.com>
2019-10-28 17:27:04 +0800

    Register routes in index.js

--- src/index.js
+++ src/index.js
@@ -8,17 +8,19 @@ const session = require('koa-session')
 
 const socket = require('./middlewares/socket')
 
+const testRouter = require('./routes/test')
+const chatRouter = require('./routes/chat')
+const userRouter = require('./routes/user')
+const sessionRouter = require('./routes/session')
+
 const config = require('./config')
 const database = require('./database')
-const api = require('./route')
 
-
-const app = new Koa()
-const router = new Router()
 
 // 連接資料庫
 database.connect()
 
+const app = new Koa()
 app.keys = config.app.keys
 
 // 供網頁前端開發時解除 cors 限制
@@ -49,7 +51,12 @@ app.use(session({
 }, app))
 app.use(socket.middleware())
 
-router.use('/api', api.router.routes(), api.router.allowedMethods())
+// 註冊路由
+const router = new Router()
+router.use('/api/', testRouter.routes(), testRouter.allowedMethods())
+router.use('/api/chat', chatRouter.routes(), chatRouter.allowedMethods())
+router.use('/api/user', userRouter.routes(), userRouter.allowedMethods())
+router.use('/api/session', sessionRouter.routes(), sessionRouter.allowedMethods())
 app.use(router.routes()).use(router.allowedMethods())
 
 const server = socket.setupServer(app)
