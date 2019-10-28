41e8633b7111fbd422b2004c75be5a6696353d7f
Pin Lin <moneycat711@gmail.com>
2019-10-28 14:10:33 +0800

    Beautify code of chatrooms.js

--- src/models/chatrooms.js
+++ src/models/chatrooms.js
@@ -1,20 +1,22 @@
 const mongoose = require('mongoose')
-
 const Schema = mongoose.Schema
+
 // 定義 user 儲存結構
 let user = new Schema({
     username: { type: String, index: true, minlength: 3, maxlength: 30, default: '' },
-    messageKey: { type: String, index: true, default: '' }
+    messageKey: { type: String, index: true, default: '' },
 })
+
 // 定義 message 儲存結構
 let message = new Schema({
     id: { type: Number, index: true, default: 1 },
-    timestamp: { type: String, default: '' },
     sender: { type: String, index: true, default: '' },
     type: { type: String, index: true, default: '' },
     content: { type: String, index: true, default: '' },
-    options: { type: Object, index: true, default: {} }
+    options: { type: Object, index: true, default: {} },
+    timestamp: { type: String, default: '' },
 })
+
 // 定義 chatroom 儲存結構
 let chatroom = new Schema({
     messages: [message],
@@ -22,6 +24,7 @@ let chatroom = new Schema({
     userA: { type: user, index: { unique: true, dropDups: true } },
     userB: { type: user, index: { unique: true, dropDups: true } }
 })
+
 // 對 chatroom 加入靜態方法，讓他被建立成模組後可以直接調用
 chatroom.statics.findByUsersname = function (users, fields) {
     return this.findOne({
@@ -29,6 +32,7 @@ chatroom.statics.findByUsersname = function (users, fields) {
         'userB.username': users.userB.username
     }, fields)
 }
+
 // 建立 chatroom 模組
 const model = mongoose.model('chatroom', chatroom)
 
